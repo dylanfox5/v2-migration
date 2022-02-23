75c516b574df1b9ae68113f36a4d2c30f1eede12
Dylan Fox <dylan.fox.kc@gmail.com>
2022-02-22 20:52:13 -0600

    finish about me copy

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,22 +11,26 @@ function About() {
                 <Col sm={12} lg={6}>
                     <h1 className='about-h'>about-me</h1>
                     <p>
-                        Leadership // Innovation // Problem-Solving
+                        Leadership <span>//</span> Innovation <span>//</span> Problem-Solving
                     </p>
                     <p>
                         I'm a strong advocate for serving others; choosing to have
                         the humility to put others first and care for others. A good leader must first be a good follower.
-                        These are the foundations of leadership.
+                        These are the foundations of <span>leadership</span>.
                     </p>
                     <p>
                         Continuously inventing and reinventing how I can serve others & society. The term
                         Futurist is one I often use to describe myself. The future and its possibilities excite me.
-                        I think our potential as humans are limitless when innovation is at the forefront.
+                        I think our potential as humans are limitless when <span>innovation</span> is at the forefront.
                     </p>
                     <p>
                         The art of understanding an obstacle, how it works, and how it can be overcome, is one
                         I find nothing short of beautiful. The leaders that bring forth our future and open its 
-                        possibilities are nothing without the vision of problem-solving.
+                        possibilities are nothing without the vision of <span>problem-solving</span>.
+                    </p>
+                    <p>
+                        This is the ideology I live each day by. Be sure to continue on or follow me on any social media
+                        to see what that looks like. 🚀
                     </p>
                 </Col>
                 <Col sm={12} lg={6}>
