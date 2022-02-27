a7b8b1244858b0d0de182a8f2ccf9016256eb315
Dylan Fox <dylan.fox.kc@gmail.com>
2022-02-26 22:39:12 -0600

    Add cards to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './Projects.css';
 
 function Projects() {
@@ -8,6 +10,42 @@ function Projects() {
         <Container className='projects'>
             <Row>
                 <h1 className='projects-h'>featured-projects</h1>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>v2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>v2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>v2</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
         </Container>
     );
