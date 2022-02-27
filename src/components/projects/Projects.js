91137ce0b73951aa5cb31fda7dedf74cd7105725
Dylan Fox <dylan.fox.kc@gmail.com>
2022-02-26 22:42:04 -0600

    update card titles

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -25,7 +25,7 @@ function Projects() {
                 <Col>
                     <Card>
                         <Card.Body>
-                            <Card.Title>v2</Card.Title>
+                            <Card.Title>v1</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
@@ -37,7 +37,7 @@ function Projects() {
                 <Col>
                     <Card>
                         <Card.Body>
-                            <Card.Title>v2</Card.Title>
+                            <Card.Title>Graceland Today</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
